70bfd418eeab09417ddb2ee65f93368871f25019
Deuxx2025 <cristhianloa15@gmail.com>
2026-07-04 18:01:38 +0100

    Fixed player movement, player still hovers the ground

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,26 +1,71 @@
+using NUnit.Framework;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     PlayerInput playerInput;
     InputAction moveAction;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector3 velocity;
+    public float speed = 12f;
+    public float gravity = 9.81f * 2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    bool isMoving;
+
+    private Vector3 lastPosition = new Vector3(0f,0f,0f);
+    private CharacterController controller;
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        movePlayer();
-    }
+        Debug.Log(isGrounded);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-    void movePlayer()
-    {
-        Debug.Log(moveAction.ReadValue<Vector2>());
+        Vector2 getPosition = moveAction.ReadValue<Vector2>();
+
+        float x = getPosition.x;
+        float y = getPosition.y;
+
+        if (isGrounded && velocity.y <= 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity);
+            Debug.Log("jumped");
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (lastPosition != gameObject.transform.position && isGrounded == true)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false; 
+        }
+
+        lastPosition = gameObject.transform.position;
     }
 }

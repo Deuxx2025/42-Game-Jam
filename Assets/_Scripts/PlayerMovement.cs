5cf522a6d608c75a2277c1c23a8f35d6ac0ee443
Deuxx2025 <cristhianloa15@gmail.com>
2026-07-04 04:06:41 +0100

    Added the player inputs for movement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    PlayerInput playerInput;
+    InputAction moveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        Debug.Log(moveAction.ReadValue<Vector2>());
     }
 }
